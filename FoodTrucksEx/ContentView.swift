478a52d6457faf338f1197fd0d2bf4bac447fda9
Hai Nguyen <hnguyen@signifyhealth.com>
2019-08-27 23:16:07 -0500

    Initial Commit

---
+++ FoodTrucksEx/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTrucksEx
+//
+//  Created by Hai Nguyen on 8/27/19.
+//  Copyright © 2019 Hai Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
